b99b80a9389fbca1ab4897708d4727c7ae948c31
Salvinus01 <86074085+Salvinus01@users.noreply.github.com>
2021-06-30 19:44:04 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Google_Labs_Tip_Time
+ Laboratório de exercícios proposto pelo Google Developers
---
+++ app/src/main/java/br/com/google/Google_Labs_Tip_Time/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.google.Google_Labs_Tip_Time
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
