6c6effa1962f19146a4c367cd42eb77563cc808f
Salvinus01 <86074085+Salvinus01@users.noreply.github.com>
2021-07-01 12:46:10 -0300

    Código Implementado

--- app/src/main/java/br/com/google/Google_Labs_Tip_Time/MainActivity.kt
+++ app/src/main/java/br/com/google/Google_Labs_Tip_Time/MainActivity.kt
@@ -1,12 +1,44 @@
 package br.com.google.Google_Labs_Tip_Time
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.google.Google_Labs_Tip_Time.R
+import androidx.appcompat.app.AppCompatActivity
+import br.com.google.Google_Labs_Tip_Time.databinding.ActivityMainBinding
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
     }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null || cost == 0.0){
+            displayTip(0.0)
+            return
+        }
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+        if (binding.roundUpSwitch.isChecked) {
+            tip = ceil(tip)
+        }
+        displayTip(tip)
+    }
+
+    private fun displayTip (tip: Double){
+        val formatedTip = java.text.NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.gorjeta, formatedTip)
+    }
+
 }
