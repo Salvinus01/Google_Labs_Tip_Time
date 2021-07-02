7791d9c8966566d8aa751e8aeb1a49f5e229e8b5
Salvinus01 <86074085+Salvinus01@users.noreply.github.com>
2021-07-02 13:53:36 -0300

    Layout refinado. Experiência do usuário melhorada.

--- app/src/main/java/br/com/google/Google_Labs_Tip_Time/MainActivity.kt
+++ app/src/main/java/br/com/google/Google_Labs_Tip_Time/MainActivity.kt
@@ -1,6 +1,10 @@
 package br.com.google.Google_Labs_Tip_Time
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import br.com.google.Google_Labs_Tip_Time.databinding.ActivityMainBinding
 import kotlin.math.ceil
@@ -14,7 +18,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
-    }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)}
+
+        }
 
     private fun calculateTip() {
         val stringInTextField = binding.costOfServiceEditText.text.toString()
@@ -25,8 +31,8 @@ class MainActivity : AppCompatActivity() {
         }
         val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
             R.id.option_twenty_percent -> 0.20
-            R.id.option_eighteen_percent -> 0.18
-            else -> 0.15
+            R.id.option_eighteen_percent -> 0.15
+            else -> 0.10
         }
 
         var tip = tipPercentage * cost
@@ -39,6 +45,17 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip (tip: Double){
         val formatedTip = java.text.NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.gorjeta, formatedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Oculta o teclado virtual ao dar <enter>
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 
 }
